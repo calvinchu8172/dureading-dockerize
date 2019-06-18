3088eb71ca1a5ac52ceefbaf70c16a3a99854b27
Calvin Chu <calvinchu@ecoworkinc.com>
2019-06-18 16:23:06 +0800

    1. Implement Bootstrap in header and books index page. 2. Add Bootstrap Flash Message helper. 3. Add Flash Message in welcome page temporarily, will adjust later.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def flash_message
+    boostrap_alert_type = ['success','info','warning','danger']
+    content_tag :div, class: "flash-messages" do
+      flash.map do |key, value| 
+        unless boostrap_alert_type.include?(key)
+          key = 'info'
+        end
+        content_tag :div, class: "alert alert-dismissable alert-#{key} info-custom" do 
+          content_tag(:span, '&times;'.html_safe, class: :close, 'data-dismiss' => 'alert') + value
+        end
+      end.join().html_safe
+    end
+  end
+
 end
